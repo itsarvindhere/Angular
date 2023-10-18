2f62333fb233edcaf7b3916e447ca393dfac972a
Arvind <ranaarvind156@gmail.com>
2023-10-18 17:35:18 +0530

    Template Driven Forms Contd.

--- 006. Handling Forms in Angular/Template-driven Forms/forms-td-final/src/app/app.component.ts
+++ 006. Handling Forms in Angular/Template-driven Forms/forms-td-final/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,14 +7,21 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('f')
+  signupForm: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
   // When Form is submitted, this method gets called
-  onSubmit(f: NgForm) {
-    console.log("Form Submitted!", f);
-  }
+  // onSubmit(f: NgForm) {
+  //   console.log("Form Submitted!", f);
+  // }
 
+    onSubmit() {
+      console.log(this.signupForm);
+    }
 
 }
