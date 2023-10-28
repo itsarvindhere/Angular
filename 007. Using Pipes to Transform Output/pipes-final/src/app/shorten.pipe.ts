f890dbeb5eac5b40c29a471d1e3d4ff6bb3e408d
Arvind <ranaarvind156@gmail.com>
2023-10-28 15:15:27 +0530

    Creating Custom Pipes

---
+++ 007. Using Pipes to Transform Output/pipes-final/src/app/shorten.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'shorten'
+})
+export class ShortenPipe implements PipeTransform {
+
+  transform(value: string, limit: number, anotherArgument: any): unknown {
+    console.log("Another arugument is ", anotherArgument);
+    if (value.length > limit) {
+      return value.substring(0,limit) + "...";
+    }
+    return value
+  }
+}
