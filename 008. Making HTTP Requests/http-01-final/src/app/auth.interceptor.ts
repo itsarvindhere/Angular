237424608ff8c9c4465177e492aebf633f94ffba
Arvind <ranaarvind156@gmail.com>
2023-11-04 18:57:36 +0530

    Intercepting the Requests and Responses

--- 008. Making HTTP Requests/http-01-final/src/app/auth.interceptor.ts
+++ 008. Making HTTP Requests/http-01-final/src/app/auth.interceptor.ts
@@ -3,9 +3,13 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpHeaders,
+  HttpEventType
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs-compat/operator/map';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -15,7 +19,27 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     console.log("Request is on its way", request)
+    // request.headers.append("auth-header", "123");
+    
+    const modifiedRequest = request.clone({
+      // To the existing headers, just append a new one
+      // headers: request.headers.append("auth-header", "123"),
+      setHeaders: {
+        "auth-header0": "123",
+        "auth-header1": "123",
+        "auth-header2": "123",
+      }
+    });
 
-    return next.handle(request);
+    console.log("Modified REquest is", modifiedRequest)
+
+    // You always get an "event" as the response here
+    // So that you have a granular access to the response
+    return next.handle(modifiedRequest).pipe(tap(event => {
+     if(event.type === HttpEventType.Response) {
+      console.log("Response arrived")
+      console.log("Body is", event.body);
+     }
+    }));
   }
 }
