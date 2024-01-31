f017ddccf39680849229b6a63f3ebbb52c4a2c16
Arvind <ranaarvind156@gmail.com>
2024-01-31 16:59:33 +0530

    Service Workers

---
+++ 015. Service Workers/angular-service-workers-final/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, isDevMode } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+@NgModule({
+  declarations: [AppComponent, PostComponent],
+  imports: [BrowserModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
