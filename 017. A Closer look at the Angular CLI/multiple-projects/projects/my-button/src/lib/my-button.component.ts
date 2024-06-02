eabd72b7db6a0468d9ba147c86a6938544918325
Arvind <ranaarvind156@gmail.com>
2024-06-02 20:22:53 +0530

    Managing multiple Angular Applications & Libraries

---
+++ 017. A Closer look at the Angular CLI/multiple-projects/projects/my-button/src/lib/my-button.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-button',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      my-button works!
+    </p>
+  `,
+  styles: ``
+})
+export class MyButtonComponent {
+
+}
---
+++ 017. A Closer look at the Angular CLI/multiple-projects/projects/my-button/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of my-button
+ */
+
+export * from './lib/my-button.service';
+export * from './lib/my-button.component';
