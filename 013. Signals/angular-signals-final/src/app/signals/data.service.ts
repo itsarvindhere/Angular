05753f43abb72f2dae828fa99ac70c5e0c61208c
Arvind <ranaarvind156@gmail.com>
2024-12-01 19:41:53 +0530

    Using Signals for Cross Component Communication

--- 013. Signals/angular-signals-final/src/app/signals/data.service.ts
+++ 013. Signals/angular-signals-final/src/app/signals/data.service.ts
@@ -1,18 +1,21 @@
-import { Injectable, Signal, signal } from '@angular/core';
+import { Injectable, Signal, signal, computed } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  
-  submittedValue = signal<string>('');
+  // Private so that it cannot be modified directly from outside
+  private _submittedValue = signal<string>('');
+
+  // Public readonly computed signal that can be read from outside
+  submittedValue = computed(() => this._submittedValue());
 
   constructor() { 
   }
 
   updateValue(val: string) {
-    this.submittedValue.set(val);
+    this._submittedValue.set(val);
   }
 
 }
