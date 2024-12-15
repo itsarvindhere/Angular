d8b71148905d5071553fbe68aa69e766ba4dfa57
Arvind <ranaarvind156@gmail.com>
2024-12-15 19:29:11 +0530

    Signals vs Observables

--- 005. Understanding Observables/obs-01-final/src/app/app.component.ts
+++ 005. Understanding Observables/obs-01-final/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, signal, inject, Injector, Signal } from '@angular/core';
 import { UserService } from './user.service';
-import { Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
+import {toObservable, toSignal} from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,32 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  constructor(private userService: UserService) {}
+  count = signal(0);
+  // count$ = toObservable(this.count);
+  count$: Observable<number>;
+
+  private injector = inject(Injector);
+
+  interval$ = interval(1000);
+  intervalSignal: Signal<number>;
+
+  constructor(private userService: UserService) {
+    // this.count$ = toObservable(this.count);
+    // this.intervalSignal = toSignal(this.interval$);
+  }
 
   userActivated = false;
   activatedSub: Subscription;
 
   ngOnInit() {
+
+    this.count$ = toObservable(this.count, {injector: this.injector});
+
+    this.intervalSignal = toSignal(this.interval$, {
+      injector: this.injector,
+      initialValue: 1
+    });
+
     this.activatedSub = this.userService.activatedEmitter.subscribe(data => {
       this.userActivated = data;
     })
