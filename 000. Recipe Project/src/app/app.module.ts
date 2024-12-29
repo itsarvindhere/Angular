d4141b2988d80bca42029937288138a08b52b036
Arvind <ranaarvind156@gmail.com>
2024-12-29 18:13:31 +0530

    Update Angular version to 18

--- 000. Recipe Project/src/app/app.module.ts
+++ 000. Recipe Project/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,19 +7,10 @@ import { HeaderComponent } from './header/header.component';
 import { AuthInterceptor } from './auth/auth.interceptor';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from './shared/shared.module';
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
-  ],
-  bootstrap: [AppComponent]
+@NgModule({ 
+    declarations: [ AppComponent, HeaderComponent],
+    bootstrap: [AppComponent], 
+    imports: [BrowserModule,AppRoutingModule], 
+    providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }, provideHttpClient(withInterceptorsFromDi())] 
 })
 export class AppModule { }
