ab972ab4b431280f95efe1d8566cde14a5d4cc8d
Arvind <ranaarvind156@gmail.com>
2026-03-14 20:07:32 +0530

    Field Classes

--- 006. Handling Forms in Angular/Signal Forms/signal-forms/src/app/app.config.ts
+++ 006. Handling Forms in Angular/Signal Forms/signal-forms/src/app/app.config.ts
@@ -2,10 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideSignalFormsConfig } from '@angular/forms/signals';
+import { NG_STATUS_CLASSES } from '@angular/forms/signals/compat';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideSignalFormsConfig({
+      classes: NG_STATUS_CLASSES
+    })
   ]
 };
